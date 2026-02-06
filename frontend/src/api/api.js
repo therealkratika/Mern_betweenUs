a3e7d4824af95be48a131ca8522f9ac93e58a137
Kratika Gupta <therealkratikagupta@gmail.com>
2026-02-06 22:27:53 +0530

    Fix frontend API URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://mern-betweenus-c1c7.onrender.com",
   headers: {
     "Content-Type": "application/json"
   }
