003173f31af3a4dd715f350ef89108a183f34f22
Kratika Gupta <therealkratikagupta@gmail.com>
2026-02-08 13:12:48 +0530

    fixing error

--- betweenUs_backend/server.js
+++ betweenUs_backend/server.js
@@ -2,50 +2,53 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
+
 const app = express();
+
+// Routes
 const authRoutes = require("./src/routes/authRoutes");
 const spaceRoutes = require("./src/routes/spaceRoutes");
 const dayMemoryRoutes = require("./src/routes/dayMemoryRoutes");
-const path = require("path");
+const uploadRoutes = require("./src/routes/uploadRoutes");
+const letterRoutes = require("./src/routes/letterRoutes");
 
+// Cron
+require("./src/cron/deleteAccount");
+
+// CORS
 const allowedOrigins = [
-    "http://localhost:5173",
-  "https://mern-betweenus-1.onrender.com"
+  "http://localhost:5173",
+  "https://mern-betweenus-1.onrender.com" 
 ];
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
+// API routes
 app.use("/auth", authRoutes);
 app.use("/spaces", spaceRoutes);
 app.use("/memories", dayMemoryRoutes);
-app.use("/upload", require("./src/routes/uploadRoutes"));
-app.use("/letters", require("./src/routes/letterRoutes"));
-app.get("/",(req,res)=>{
-    res.send("BetweenUs bakend running")
-});
-require("./src/cron/deleteAccount");
+app.use("/upload", uploadRoutes);
+app.use("/letters", letterRoutes);
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
+// Health check 
+app.get("/health", (req, res) => {
+  res.json({ status: "Backend running" });
+});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     console.log("MongoDB connected");
-    app.listen(5000,()=>{
-        console.log("server is running on port 5000");
+    app.listen(process.env.PORT || 5000, () => {
+      console.log("Server running");
     });
-   app.use(express.static(path.join(__dirname, "dist")));
-
-app.use((req, res) => {
-  res.sendFile(
-    path.join(__dirname, "dist/index.html")
-  );
-});
-
-}).catch((err)=>{
-    console.error("DB connection failed",err);
-});
-
-
-
+  })
+  .catch((err) => {
+    console.error("DB connection failed:", err);
+  });
