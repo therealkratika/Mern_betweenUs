7c3a7820d77a30594cc09e8a9b8c3db8f81e7a5f
Kratika Gupta <therealkratikagupta@gmail.com>
2026-02-20 23:25:59 +0530

    frontend netlify

--- betweenUs_backend/server.js
+++ betweenUs_backend/server.js
@@ -16,7 +16,7 @@ require("./src/cron/deleteAccount");
 // CORS
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://mern-betweenus-1.onrender.com" 
+  "https://69989d5adb605fed74a66e84--betweenuss.netlify.app/" 
 ];
 
 app.use(
